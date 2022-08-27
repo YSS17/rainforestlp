537a2ebc32ca8685c51248424201426e9c7cd2e7
Yan Santos <yan17souza@hotmail.com>
2022-08-27 03:38:07 -0300

    fixing styles @ sidebar menu

--- src/components/sidebar/index.js
+++ src/components/sidebar/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { SidebarContainer, Icon, CloseIcon } from './sidebar';
+import { SidebarContainer, Icon, CloseIcon,
+         SidebarWrapper, SidebarMenu, SidebarLink, SideBtnWrap, 
+         SidebarRoute} from './sidebar';
 
 const Sidebar = () => {
   return (
@@ -7,6 +9,17 @@ const Sidebar = () => {
       <Icon>
         <CloseIcon />
       </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">About</SidebarLink>
+          <SidebarLink to="discover">Discover</SidebarLink>
+          <SidebarLink to="services">Services</SidebarLink>
+          <SidebarLink to="signup">Sign up</SidebarLink>
+        </SidebarMenu>
+        <SideBtnWrap>
+          <SidebarRoute to="/signin">Sign in</SidebarRoute>
+        </SideBtnWrap>
+      </SidebarWrapper>
     </SidebarContainer>
   )
 }
