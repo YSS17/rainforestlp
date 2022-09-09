e82a722214e42f6a673df6653bab6c366e4b424b
Yan Santos <yan17souza@hotmail.com>
2022-09-09 17:42:31 -0300

    adding typewriter effect via npm and creating text for typewriter at hero section

--- src/components/heroSection/index.js
+++ src/components/heroSection/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Video from '../../videos/video.mp4';
+import Typewriter from 'typewriter-effect';
 import { Button } from '../buttonElement';
 import { HeroContainer, HeroBg, VideoBg,
          HeroContent, HeroH1, HeroP,
@@ -19,8 +20,28 @@ const HeroSection = () => {
         <VideoBg autoPlay loop muted src={Video} type='video/mp4'/>
       </HeroBg>
       <HeroContent>
-        <HeroH1>Exemple Exemple Exemple</HeroH1>
-        <HeroP>Sign up for new account today</HeroP>
+        <HeroH1>        
+          <Typewriter onInit={(typewriter) => {
+            typewriter
+            .pauseFor(500)
+            .typeString("healthcare ")
+            .pauseFor(500)
+            .typeString('<span style="color: #01bf71;">Sustainable</span>')
+            .pauseFor(800)
+            .deleteChars(11)
+            .typeString('<span style="color: #01bf71;">from Amazon</span>')
+            .pauseFor(800)
+            .deleteChars(11)
+            .typeString('<span style="color: #01bf71;">Premium</span>')
+            .pauseFor(800)
+            .deleteAll()
+            .pauseFor(500)
+            .typeString('<span style="color: #01bf71;">Nutrimatilha</span>')
+            .start()
+          }}
+          />
+        </HeroH1>
+        <HeroP>Get access to our plans</HeroP>
         <HeroBtnWrapper>
           <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>Get Started {hover ? <ArrowForward/> : <ArrowRight/>}</Button>
         </HeroBtnWrapper>
